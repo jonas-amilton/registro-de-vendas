75e1ee8499d014f25c0f4ca30bade238f69cf7a8
jonas-amilton <jonas96@edu.unisinos.br>
2024-09-24 00:22:28 -0300

    chore: Alterando titulo da aplicação

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -74,7 +74,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
     <footer id="footer" class="mt-auto py-3 bg-light">
         <div class="container">
             <div class="row text-muted">
-                <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
+                <div class="col-md-6 text-center text-md-start">&copy; Registro de Vendas <?= date('Y') ?></div>
                 <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
             </div>
         </div>
