7b5bff178ac49f26b209668124c6de6b1c2c91d2
jonas-amilton <jonas96@edu.unisinos.br>
2024-09-24 01:25:27 -0300

    refactor: Alterado o visual da home e adicionado verificação de login na sua action

--- views/site/index.php
+++ views/site/index.php
@@ -2,50 +2,57 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+use yii\helpers\Url;
+
+$this->title = 'Home | Registro de Vendas';
 ?>
 <div class="site-index">
 
     <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
+        <h1 class="display-4">Bem Vindo!</h1>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+        <p class="lead">Sistema de Registro de Vendas.</p>
     </div>
 
     <div class="body-content">
 
         <div class="row">
             <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
+                <div class="card">
+                    <div class="card-body">
+                        <h2>Clientes Registrados</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                        <p class="text-danger">Gerencie os clientes registrados.</p>
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+                        <p><a class="btn btn-outline-secondary" href="<?= Url::to('/customer'); ?>">Acessar
+                                &raquo;</a></p>
+                    </div>
+                </div>
             </div>
             <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
+                <div class="card">
+                    <div class="card-body">
+                        <h2>Itens Registrados</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                        <p class="text-danger">Gerencie os itens registrados.</p>
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+                        <p><a class="btn btn-outline-secondary" href="<?= Url::to('/sales-item'); ?>">Acessar
+                                &raquo;</a>
+                        </p>
+                    </div>
+                </div>
             </div>
             <div class="col-lg-4">
-                <h2>Heading</h2>
+                <div class="card">
+                    <div class="card-body">
+                        <h2>Lista de Vendas</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+                        <p class="text-danger">Gerencie as vendas registradas.</p>
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+                        <p><a class="btn btn-outline-secondary" href="<?= Url::to('/sales-list'); ?>">Acessar
+                                &raquo;</a></p>
+                    </div>
+                </div>
             </div>
         </div>
 
